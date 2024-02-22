efb69bc588118cda7be2ed686680449c4d8a6dea
GuestFromSpaceX <gfromsx@gmail.com>
2024-02-22 21:51:59 +0300

    5 task done

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,12 @@
 function truncate(str, maxlength) {
-  // ваш код...
+  let dots = '…';
+  let array = [];
+  if (str.length <= maxlength) {
+    return str;
+  } else {
+    for (let i = 0; i < maxlength - 1; i++) {
+      array.push(str[i]);
+    }
+    return array.join('') + dots;
+  }
 }
