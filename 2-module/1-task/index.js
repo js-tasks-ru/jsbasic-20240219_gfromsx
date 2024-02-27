c85c42b7273c66d53a658c8ad5e3e79e3ff8b05c
Simon Ermolinsky <gfromsx@gmail.com>
2024-02-27 15:18:46 +0300

    2 module 1 task done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let counter = 0;
+  for (let key in salaries) {
+    if (isFinite(salaries[key])) {
+      counter += salaries[key];
+    }
+  }
+  return counter;
 }
