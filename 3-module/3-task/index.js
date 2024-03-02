c766e8d11bfab104530def1c0351a6e12e37bd0d
Simon Ermolinsky <gfromsx@gmail.com>
2024-03-02 17:05:19 +0300

    module 3, 1-3 tasks done

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
 function camelize(str) {
-  // ваш код...
+  str = str.split('');
+  while (str.includes('-')) {
+    let index = str.indexOf('-');
+    if (index !== -1) {
+      str[index] = '';
+      str[index + 1] = str[index + 1].toUpperCase();
+    }    
+  }
+  return (str.join(''));
 }
