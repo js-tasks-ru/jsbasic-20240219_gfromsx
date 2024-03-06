d35b6fd41f643b87a6a09a01031adc09cdc21228
Simon Ermolinsky <gfromsx@gmail.com>
2024-03-06 18:45:55 +0300

    fix

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,40 @@
-function highlight(table) {
-  // ваш код...
+function highlight(table, tbody) {
+  let classAvailable = 'available';
+  let classUnavailable = 'unavailable';
+  
+  //выбираю строки таблицы
+  let trElements = tbody.getElementsByTagName('TR');
+  
+  //делаю их авелибл и анавелибл
+  for (let trElement of trElements) {
+    for(let tdElement of trElement.children) {
+      if (tdElement.dataset.available == 'true') {
+        tdElement.parentElement.classList.add(classAvailable);
+      } 
+      if (tdElement.dataset.available == 'false') {
+        tdElement.parentElement.classList.add(classUnavailable);
+      }
+    };
+  }
+
+  //скрываю лишние строки
+  for (let trElement of trElements) {
+    if (trElement.className == '') {
+      trElement.hidden = true;
+    }
+  }
+
+  //добавляю разделение на гендеры
+  for (let trElement of trElements) {
+    for(let tdElement of trElement.children) {
+      if (tdElement.textContent == 'm') {
+        tdElement.parentElement.classList.add('male');
+      }
+      if (tdElement.textContent == 'f') {
+        tdElement.parentElement.classList.add('female');
+      }
+    };
+  }
+
+  return console.log(trElements)
 }
