1db149e7efba22d13ba61e012a8b6ae57e05c9b8
GuestFromSpaceX <gfromsx@gmail.com>
2024-03-13 14:15:03 +0300

    5 module 3 task done

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,50 @@
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector('.carousel__inner');
+  let rightArrow = document.querySelector('.carousel__arrow_right');
+  let leftArrow = document.querySelector('.carousel__arrow_left');
+  let carouselCounter = 0;
+
+  if (carouselCounter == 0) {
+    leftArrow.style.display = 'none';   
+  } else if (carouselCounter == 3) {
+    rightArrow.style.display = 'none';
+  } else {
+    leftArrow.style.display = '';
+    rightArrow.style.display = '';
+  }
+
+  rightArrow.addEventListener('click', function () {
+    carouselCounter++;
+    if (carouselCounter == 3) {
+      rightArrow.style.display = 'none';   
+    } else {
+      rightArrow.style.display = '';
+    }  
+    if (carouselCounter == 0) {
+      leftArrow.style.display = 'none';   
+    } else {
+      leftArrow.style.display = '';
+    } 
+    console.log(carouselCounter);
+    carousel.style.transform = `translateX(-${carousel.offsetWidth * carouselCounter}px)`;
+    console.log(carouselCounter);
+  });
+
+  leftArrow.addEventListener('click', function () {
+    carouselCounter--;
+    if (carouselCounter == 3) {
+      rightArrow.style.display = 'none';   
+    } else {
+      rightArrow.style.display = '';
+    }  
+    if (carouselCounter == 0) {
+      leftArrow.style.display = 'none';   
+    } else {
+      leftArrow.style.display = '';
+    }  
+    console.log(carouselCounter);
+    carousel.style.transform = `translateX(-${carousel.offsetWidth * carouselCounter}px)`;
+    console.log(carouselCounter);
+  });
+
 }
