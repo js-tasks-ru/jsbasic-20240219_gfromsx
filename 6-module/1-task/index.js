6ef03dbf148bf9ceb44fd5a7f81ac027d750da5c
GuestFromSpaceX <gfromsx@gmail.com>
2024-03-22 14:37:13 +0300

    6 module 1 task done?

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,43 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+
+    this.render();
+    this.addEventListener();
+  }
+
+  render() {
+    let table = document.createElement('TABLE');
+
+    table.innerHTML = `<thead>
+                          <tr>
+                              <th>Имя</th>
+                              <th>Возраст</th>
+                              <th>Зарплата</th>
+                              <th>Город</th>
+                              <th></th>
+                          </tr>
+                      </thead>` ;
+    table.innerHTML += `<tbody>${this.rows.map(({name, age, salary, city}) => `<tr>
+    <th>${name}</th>
+    <th>${age}</th>
+    <th>${salary}</th>
+    <th>${city}</th>
+    <th><button>X</button></th>
+    </tr>`).join('')}</tbody>`;
+
+    this.elem = table;
+  }
+
+  addEventListener() {
+    this.elem.addEventListener('click', function(event) {
+      let button = event.target.closest('BUTTON');
+      let th = event.target.closest('TH');
+      
+      if (button) {
+        th.parentElement.hidden = 'true';
+      }
+    });
   }
 }
